c5dab1e1d873fe42d8c7b63b7eeab8d1086731f1
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2021-09-06 19:56:20 +0200

    fit: gpx

--- fit/main.go
+++ fit/main.go
@@ -15,16 +15,32 @@ type recfunc func(fit.RecordMsg) string
 func (f recfunc) String(r fit.RecordMsg) string { return f(r) }
 
 func main() {
-	if len(os.Args) < 2 {
-		fatal(fmt.Errorf("fit file.fit..\nktye/kyd/fit/main.go"))
+	a := os.Args[1:]
+	f, h, t, r := recprint, "", "", false
+	if len(a) > 1 && a[0] == "gpx" {
+		a = a[1:]
+		f = gpxprint
+		h = `<?xml version="1.0" encoding="UTF-8"?>
+<gpx version="1.0"><trk><trkseg>` + "\n"
+		t = `</trkseg></trk></gpx>` + "\n"
 	}
-	for _, file := range os.Args[1:] {
-		Fit(file, recprint)
+	if len(a) > 1 && a[0] == "3x" {
+		a = a[1:]
+		r = true
+	}
+	if len(a) < 1 {
+		fatal(fmt.Errorf("fit file.fit..\nfit gpx file.fit..\nktye/kyd/fit/main.go"))
+	}
+	for _, file := range a {
+		Fit(file, f, h, t, r)
 	}
 }
 func recprint(r *fit.RecordMsg) {
 	// https://pkg.go.dev/github.com/tormoder/fit#RecordMsg
 	fmt.Println(r.Timestamp.Format("2006.01.02T15:04:05"), r.GetDistanceScaled()/1000.0, r.PositionLat.Degrees(), r.PositionLong.Degrees(), r.GetEnhancedAltitudeScaled(), r.HeartRate)
+}
+func gpxprint(r *fit.RecordMsg) {
+	fmt.Printf("<trkpt lat=\"%.8f\" lon=\"%.8f\"><time>%s</time></trkpt>\n", r.PositionLat.Degrees(), r.PositionLong.Degrees(), r.Timestamp.Format("2006-01-02T15:04:05Z"))
 }
 func fatal(e error) {
 	if e != nil {
@@ -35,7 +51,7 @@ func ftoa(f float64) string { return strconv.FormatFloat(f, 'v', -1, 64) }
 
 var itoa = strconv.Itoa
 
-func Fit(file string, recprint func(r *fit.RecordMsg)) {
+func Fit(file string, recprint func(r *fit.RecordMsg), head, tail string, reduce bool) {
 	b, e := ioutil.ReadFile(file)
 	fatal(e)
 
@@ -55,8 +71,14 @@ func Fit(file string, recprint func(r *fit.RecordMsg)) {
 	}
 
 	rec := a.Records
-
+	os.Stdout.Write([]byte(head))
+	rr := 0
 	for _, r := range rec {
-		recprint(r)
+		if !reduce || rr == 2 {
+			recprint(r)
+			rr = -1
+		}
+		rr++
 	}
+	os.Stdout.Write([]byte(tail))
 }
