5d11ccb48b4caeb764295a5978eb99c3d5bf7fba
elmar <ktye@users.noreply.github.com>
2023-09-01 09:05:57 +0200

    k db.csv

--- k.go
+++ k.go
@@ -7,14 +7,16 @@ import (
 func K(db DB) {
 	// id type year week day dist time coords
 	// i  s    i    i    i   f    f    i
+	fmt.Println("id,type,time,dist,coords")
+	fmt.Println("i,s,f,f,i")
 	n := db.Len()
 	for i := 0; i < n; i++ {
 		h := db.Head(i)
 		id := h.Start // int32 til 2038
 		typ := sport(h.Type) + 32
-		dist := h.Seconds / 3600.0
-		time := h.Meters / 1000.0
+		time := h.Seconds / 3600.0
+		dist := h.Meters / 1000.0
 		coords := h.Samples
-		fmt.Printf("%d %c %.3f %.4f %d\n", id, typ, dist, time, coords)
+		fmt.Printf("%d,%c,%.3f,%.4f,%d\n", id, typ, time, dist, coords)
 	}
 }
